70b4338a8d98032f11ea90df33a11301e2ba4fea
Alan Rebane <rebane.alan@gmail.com>
2022-08-15 10:45:49 +0200

    Connecting the app to a database

--- todo.rb
+++ todo.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require 'sinatra/reloader' if development?
 require 'tilt/erubis'
 require "sinatra/content_for"
+require_relative "database_persistence"
 
 configure do
   enable :sessions
@@ -11,8 +11,13 @@ configure do
   set :erb, :escape_html => true
 end
 
+configure(:development) do
+  require 'sinatra/reloader'
+  also_reload "database_persistence.rb"
+end
+
 before do
-  @storage = SessionPersistence.new(session)
+  @storage = DatabasePersistence.new(logger)
 end
 
 helpers do
@@ -43,66 +48,6 @@ helpers do
     incomplete_todos.each { |todo| yield todo, todos.index(todo) }
     complete_todos.each { |todo| yield todo, todos.index(todo) }
   end
-end
-
-class SessionPersistence
-
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(list_id)
-    @session[:lists].reject! { |list| list[:id] == list_id }
-  end
-
-  def update_list_name(list_id, new_name)
-    list = find_list(list_id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << {id: id, name: todo_name, completed: false}
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |t| t[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_complete(list_id)
-    list = find_list(list_id)
-    list[:todos].each { |todo| todo[:completed] = true }
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |element| element[:id] }.max || 0
-    max + 1
-  end
-
 end
 
 def load_list(id)
@@ -231,7 +176,7 @@ post '/lists/:list_id/todos/:todo_id/destroy' do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
 
-  todo_id = params[:todo_id].to_i
+  @todo_id = params[:todo_id].to_i
   @storage.delete_todo_from_list(@list_id, @todo_id)
 
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
